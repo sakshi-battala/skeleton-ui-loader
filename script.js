8cc179189e6d9d23353f41c25f6760b96a85bfa4
Sakshi <sakshibattala6@gmail.com>
2026-05-20 12:21:26 +0530

    Initial commit: add skeleton loader demo

---
+++ script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector(".users-container");
+
+function showSkeleton() {
+  for (let index = 0; index < 9; index++) {
+    container.innerHTML += `
+        <div class="skeleton-card">
+        <div class="skeleton avatar"></div>
+        <div class="skeleton name"></div>
+        <div class="skeleton email"></div>
+        </div>
+    `;
+  }
+}
+
+async function fetchUsers() {
+  const response = await fetch("https://dummyjson.com/users?limit=9");
+  const data = await response.json();
+  const users = data.users;
+
+  container.innerHTML = "";
+  users.forEach((user) => {
+    container.innerHTML += `
+      <div class="user-card">
+        <img src="${user.image}" />
+        <h3>${user.firstName}</h3>
+        <p>${user.email}</p>
+      </div>
+    `;
+  });
+}
+
+showSkeleton();
+fetchUsers();
